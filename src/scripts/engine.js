db9623cb41a37de15688b7b027d1643da8aa7daa
vini-sant14 <st-vinicius14@outlook.com>
2024-11-18 21:54:46 -0300

    finalização do jogo da memória

---
+++ src/scripts/engine.js
@@ -0,0 +1,58 @@
+const emojis = [
+    "😎",
+    "😎",
+    "🤣",
+    "🤣",
+    "🤦‍♀️",
+    "🤦‍♀️",
+    "🎂",
+    "🎂",
+    "😜",
+    "😜",
+    "🐱‍🐉",
+    "🐱‍🐉",
+    "😍",
+    "😍",
+    "✌",
+    "✌",
+];
+let openCards = [];
+
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+    if (openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+
+    }
+}
+
+function checkMatch() {
+    if(openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Você venceu!")
+    }
+}
